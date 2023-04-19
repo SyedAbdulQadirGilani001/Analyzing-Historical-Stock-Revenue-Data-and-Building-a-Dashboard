7930c1e224db1521367346beadc3cc7e6ddec6de
Abdul Qadir <17pwcse1590@uetpeshawar.edu.pk>
2023-04-19 14:49:35 +0500

    added test.py

--- test.py
+++ test.py
@@ -1,9 +1,22 @@
-import streamlit as st # import streamlit
-import seaborn as sns # import seaborn
-import pandas as pd # import pandas
-diamonds=sns.load_dataset('diamonds')  # load dataset
-st.write(diamonds.head()) # write dataset
-# describe dataset
-st.write(diamonds.describe())
-# plot dataset
-st.write(sns.pairplot(diamonds))
+# I need a website and automation of work with product descriptions in streamlit
+import streamlit as st
+import pandas as pd
+import numpy as np
+import time
+import os
+import re
+import requests
+import json
+import csv
+import datetime
+import time
+import sys
+data=np.random.randn(10,2)
+st.write(data.shape)
+st.line_chart(data[:,0])
+st.bar_chart(data[:,0])
+st.write(len(data))
+selectbox=st.sidebar.selectbox('Select',('1','2','3'))
+st.write(selectbox)
+sidebar=st.sidebar.slider('Select',0,10)
+st.write(sidebar)
